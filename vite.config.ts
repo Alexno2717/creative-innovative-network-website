5b12829c355c70da3ce1a26cac41875102c60d86
Alexno2717 <alexno2717@gmail.com>
2025-06-08 17:50:43 +1000

    fix: add @ alias in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   root: './',
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src")
+    }
+  },
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
-    emptyOutDir: true,
-    rollupOptions: {
-      input: {
-        main: './src/entry.tsx'
-      }
-    }
+    emptyOutDir: true
   }
 });
