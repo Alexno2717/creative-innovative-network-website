6527e6f8481c51e04e7151c2742f4062850df638
Alexno2717 <alexno2717@gmail.com>
2025-06-08 17:49:50 +1000

    simplify: remove GitHub Pages and fix paths

--- vite.config.ts
+++ vite.config.ts
@@ -2,16 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: '/creative-innovative-network-website/',
   plugins: [react()],
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
-    emptyOutDir: true,
-    rollupOptions: {
-      input: {
-        main: './index.html'
-      }
-    }
+    emptyOutDir: true
   }
 });
