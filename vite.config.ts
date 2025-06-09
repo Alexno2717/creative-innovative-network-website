8daef65ce58d910cff18f0251e7016100cc20540
Alexno2717 <alexno2717@gmail.com>
2025-06-09 15:11:24 +1000

    fix: move main.js to root and update script reference

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,6 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     emptyOutDir: true,
-    assetsDir: 'assets',
     rollupOptions: {
       input: {
         main: './src/entry.tsx'
